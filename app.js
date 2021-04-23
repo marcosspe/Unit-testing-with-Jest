a60f76931360b6fcda625f77b5285a9b19b0c9d1
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-23 19:38:26 +0000

    marcos

---
+++ app.js
@@ -0,0 +1,18 @@
+// const sum = (a,b) => {
+//     return a + b
+// }
+// console.log(sum(7,3))
+
+// module.exports = { sum };
+
+
+const fromEuroToDollar = (Value) => {
+    return Value * 1.2
+}
+module.exports = { fromEuroToDollar };
+
+const fromDollarToYen = (Value) => {
+    let euroadolar = value * 1.2;
+    return euroadolar * 127.9
+}
+module.exports = { fromDollarToYen };
